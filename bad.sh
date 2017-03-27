b89c72476bad8acdf50a9dbc18dee583fa6c216f
idkCpp <martin.buergmann@gmail.com>
2017-03-27 11:34:15 +0200

    Update bad.sh
    Make it not bad

--- bad.sh
+++ bad.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 var="wget bad-code.sh"
-$var
+#$var
 
-. bad-code.sh
-source bad-code.sh
+#. bad-code.sh
+#source bad-code.sh
 
-find . -type d -name "." -exec "$var" \;
+#find . -type d -name "." -exec "$var" \;
 find . -type f > dir.list
 
 good="some arguments"
@@ -17,11 +17,11 @@ if [ "$good" eq "something" ]; then
 	echo "this"
 fi
 
-echo $($var)
+#echo $($var)
 
-echo $(      		 $fishy)
+#echo $(      		 $fishy)
 # $fishy is a very bad variable
 echo Something # echo; $fishy must be caught as bad line
 
 echo $(echo Not $fishy); 
-echo I want to be accepted; $fishy
+#echo I want to be accepted; $fishy
